f843b93ebd250d60cd396515568fc5891c42a170
Greentime Hu <greentime@andestech.com>
2018-01-18 12:09:24 +0800

    change zip to tar.gz file because link issue

--- get_toolchain.sh
+++ get_toolchain.sh
@@ -4,6 +4,6 @@ work_folder=`pwd`
 src_folder="$work_folder/src_pkg"
 mkdir -p $work_folder/toolchain
 cd $work_folder/toolchain
-wget https://github.com/greentime/prebuilt-nds32-toolchain/releases/download/20180118/nds32le-linux-glibc-v3.zip
-unzip nds32le-linux-glibc-v3.zip
+wget https://github.com/greentime/prebuilt-nds32-toolchain/releases/download/20180118/nds32le-linux-glibc-v3.tar.gz
+tar zxvf nds32le-linux-glibc-v3.tar.gz
 cd -
